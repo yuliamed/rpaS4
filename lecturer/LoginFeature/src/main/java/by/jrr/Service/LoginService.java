ad740efe4d30b5b076385ba6cd4933e10325f21b
yuliamed <yulinwo.17@gmail.com>
2021-03-10 10:48:32 +0300

    adds checkUserPassword functionality

--- lecturer/LoginFeature/src/main/java/by/jrr/Service/LoginService.java
+++ lecturer/LoginFeature/src/main/java/by/jrr/Service/LoginService.java
@@ -1,5 +1,10 @@
 package by.jrr.Service;
 
+import by.jrr.bean.User;
+
 public class LoginService {
 
+    public boolean checkUserPassword(User user, String userInput) {
+        return user.getPassword().equals(userInput);
+    }
 }
